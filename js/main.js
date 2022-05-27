1513bd75ffde0e804be53a2c5b4ec78949ffa7b3
RScanlon2021 <roryscanlon21@protonmail.com>
2022-05-27 14:28:26 +0100

    All responsiveness added Footer still needed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const menuIcon = document.querySelector(".menu-icon");
 
+navToggle.addEventListener("click", (e) => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    menuIcon.style.WebkitTransitionDuration = "1s";
+    menuIcon.style.webkitTransform = "rotate(180deg)";
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    menuIcon.style.WebkitTransitionDuration = "1s";
+    menuIcon.style.webkitTransform = "rotate(0deg)";
+  }
+});
