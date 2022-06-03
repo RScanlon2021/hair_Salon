3315c42f286563f5266aec152e630737cc8bd6d4
RScanlon2021 <roryscanlon21@protonmail.com>
2022-06-03 22:37:36 +0100

    Scrolling functionality added

--- js/main.js
+++ js/main.js
@@ -17,3 +17,52 @@ navToggle.addEventListener("click", (e) => {
     menuIcon.style.webkitTransform = "rotate(0deg)";
   }
 });
+
+window.onscroll = function (e) {
+  primaryNav.setAttribute("data-visible", false);
+  navToggle.setAttribute("aria-expanded", false);
+  menuIcon.style.WebkitTransitionDuration = "1s";
+  menuIcon.style.webkitTransform = "rotate(0deg)";
+};
+
+//////SCROLLING FUNCITONALITY
+
+const scrollSections = document.querySelectorAll(".nav__link");
+
+scrollSections.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+//BACK TO TOP SCROLL BTN
+const backToTopBtn = document.querySelector(".scroll-to-top");
+
+backToTopBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+window.onresize = function (event) {
+  const icons = document.querySelectorAll(".nav-icon");
+  //   console.log("x: " + window.innerWidth + "      y: " + window.innerHeight);
+
+  for (icon of icons) {
+    if (window.innerWidth <= 880 && window.innerWidth >= 800) {
+      icon.classList.add("hide");
+    } else {
+      icon.classList.remove("hide");
+    }
+  }
+};
+
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${i * 100}%)`));
